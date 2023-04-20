2527b8f079dad437a1eb8e19ee099f70e640c5d5
John <antarcticdepths@gmail.com>
2023-04-20 19:49:47 +0200

    start proj

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Root from "./routes/root.tsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Root />} />)
+);
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <RouterProvider router={router} />
+);
---
+++ src/routes/root.tsx
@@ -0,0 +1,29 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Root() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>works</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          pLorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Consequuntur explicabo suscipit enim. Dolorum minus soluta, tempore
+          esse fuga magni expedita iure explicabo sapiente inventore
+          voluptatibus, et, saepe repellendus doloremque fugiat.
+        </div>
+        <div className="col">
+          pLorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Consequuntur explicabo suscipit enim. Dolorum minus soluta, tempore
+          esse fuga magni expedita iure explicabo sapiente inventore
+          voluptatibus, et, saepe repellendus doloremque fugiat.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Root;
