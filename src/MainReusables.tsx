16b88eb313ee42d0528d08b9ac2734c7408e008d
John <antarcticdepths@gmail.com>
2023-05-09 20:26:43 +0200

    cuystom accordion

--- src/MainReusables.tsx
+++ src/MainReusables.tsx
@@ -2,7 +2,7 @@ import "./MainReusables.css";
 import { ReactNode } from "react";
 import { Image } from "react-bootstrap";
 import Accordion from "react-bootstrap/Accordion";
-import dojoImg from "./assets/dojoImg.png";
+import dojoImg from "./assets/dojoImg1.png";
 
 interface H1ImageProps {
   src: string;
@@ -18,7 +18,13 @@ interface TextProps {
 }
 
 export function H1Image({ src, alt }: H1ImageProps) {
-  return <Image src={src} style={{ height: "2.5rem", width: "2.5rem" }} />;
+  return (
+    <Image
+      src={src}
+      rounded
+      style={{ height: "2.5rem", width: "2.5rem", marginRight: "1rem" }}
+    />
+  );
 }
 
 export function CustomAccordionHeader({ text }: TextProps) {
