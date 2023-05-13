f32f6441512aec848e4395187dbbe90a06a4cecf
John <antarcticdepths@gmail.com>
2023-05-13 17:16:00 +0200

    portfol page filter sort step 3 develop placeholder task 1

--- src/routes/portfolioData.ts
+++ src/routes/portfolioData.ts
@@ -1,6 +1,6 @@
 import dojoImg from "./../assets/f7.png";
 
-const project = {
+const Taizyu = {
   type: "App",
   mainHeader: {
     symbolImg: dojoImg,
@@ -52,6 +52,166 @@ const project = {
   },
   link: "https://awesome.com",
 };
-const portfolioData = [project];
+
+const Portfolio = {
+  type: "Website",
+  mainHeader: {
+    symbolImg: dojoImg,
+    title: "Portfolio",
+  },
+  mainImg: dojoImg,
+  shortDescription: "Portfolio",
+  valueToUser: "Shows projects",
+  subtitle: "",
+  developmentSummary: {
+    items: ["a...", "b...", "c..."],
+  },
+  metricsEstimates: {
+    items: [
+      {
+        title: "Minimize time losses",
+        score: 5,
+      },
+      {
+        title: "As simple as possible",
+        score: 5,
+      },
+      {
+        title: "Prioritize",
+        score: 5,
+      },
+      {
+        title: "ABCD contingencies vs slowdowns/roadblocks",
+        score: 5,
+      },
+      {
+        title: "Sense of development ease",
+        score: 5,
+      },
+      {
+        title: "Experience as a whole",
+        score: 5,
+      },
+    ],
+  },
+  lessonsSummary: {
+    whatWentWell: [".a..2", "..b3.", ".c.4."],
+    whatWentPoorly: ["..a1.", "..b.2", "..3.c"],
+    rootCauses: ["..1a."],
+    neverAgain: ["..a.1", ".b.12.", ".3c.."],
+  },
+  importantPoints: {
+    items: ["..a.123", ".b.4567.", ".c34543."],
+  },
+  link: "https://awesome1.com",
+};
+
+const Inaflash = {
+  type: "Website",
+  mainHeader: {
+    symbolImg: dojoImg,
+    title: "Inaflash",
+  },
+  mainImg: dojoImg,
+  shortDescription: "Inaflash",
+  valueToUser: "Electronic flashcards",
+  subtitle: "Simple, no nonsense flashcards",
+  developmentSummary: {
+    items: ["a...", "b...", "c..."],
+  },
+  metricsEstimates: {
+    items: [
+      {
+        title: "Minimize time losses",
+        score: 2,
+      },
+      {
+        title: "As simple as possible",
+        score: 2,
+      },
+      {
+        title: "Prioritize",
+        score: 2,
+      },
+      {
+        title: "ABCD contingencies vs slowdowns/roadblocks",
+        score: 4,
+      },
+      {
+        title: "Sense of development ease",
+        score: 1,
+      },
+      {
+        title: "Experience as a whole",
+        score: 1,
+      },
+    ],
+  },
+  lessonsSummary: {
+    whatWentWell: [".a..2", "..b3.", ".c.4."],
+    whatWentPoorly: ["..a1.", "..b.2", "..3.c"],
+    rootCauses: ["..1a."],
+    neverAgain: ["..a.1", ".b.12.", ".3c.."],
+  },
+  importantPoints: {
+    items: ["..a.123", ".b.4567.", ".c34543."],
+  },
+  link: "https://awesome1.com",
+};
+
+const Anglr = {
+  type: "Website",
+  mainHeader: {
+    symbolImg: dojoImg,
+    title: "Anglr",
+  },
+  mainImg: dojoImg,
+  shortDescription: "Anglr",
+  valueToUser: "Anglr proj",
+  subtitle: "Simple, no nonsense Anglr proj",
+  developmentSummary: {
+    items: ["a...", "b...", "c..."],
+  },
+  metricsEstimates: {
+    items: [
+      {
+        title: "Minimize time losses",
+        score: 4,
+      },
+      {
+        title: "As simple as possible",
+        score: 4,
+      },
+      {
+        title: "Prioritize",
+        score: 4,
+      },
+      {
+        title: "ABCD contingencies vs slowdowns/roadblocks",
+        score: 2,
+      },
+      {
+        title: "Sense of development ease",
+        score: 5,
+      },
+      {
+        title: "Experience as a whole",
+        score: 5,
+      },
+    ],
+  },
+  lessonsSummary: {
+    whatWentWell: [".a..2", "..b3.", ".c.4."],
+    whatWentPoorly: ["..a1.", "..b.2", "..3.c"],
+    rootCauses: ["..1a."],
+    neverAgain: ["..a.1", ".b.12.", ".3c.."],
+  },
+  importantPoints: {
+    items: ["..a.123", ".b.4567.", ".c34543."],
+  },
+  link: "https://awesome1.com",
+};
+
+const portfolioData = [Taizyu, Portfolio, Inaflash, Anglr];
 
 export default portfolioData;
