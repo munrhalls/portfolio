5ac004ef20fb83f166bded7015f816271e1ca176
John <antarcticdepths@gmail.com>
2023-05-16 21:35:30 +0200

    portfol about

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,5 +1,15 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 function About() {
-  return <h1>ABOUT</h1>;
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>ABOUT</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default About;
