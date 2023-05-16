780dd23432c27f8724de622316b9a2445c4f3004
John <antarcticdepths@gmail.com>
2023-05-16 22:00:09 +0200

    portfol about

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -2,40 +2,106 @@ import { Container, Row, Col } from "react-bootstrap";
 
 function About() {
   return (
-    <Container className="mt-3">
+    <Container className="mt-3 mb-3">
       <Row>
         <Col>
           <h1>Hello. I am a junior frontend web developer. </h1>
-          <p>You can learn a few things about me on this page.</p>
-          <h2>Learning software development</h2>
-          <p>How it began</p>
-          <p>Why</p>
-          <p>How did it go</p>
-          <h2>Interests in the world of software development</h2>
-          <p>Mention DS & ALGO</p>
-          <p>Geometries & gestalts</p>
-          <p>Dev rate</p>
-          <p>Conceptual exploration</p>
-          <h2>Other interests</h2>
-          <h3>Poetry</h3>
-          <p>brief intro</p>
-          <ul>
-            <li>Poem 1</li>
-            <li>Poem 2</li>
-            <li>Poem 3</li>
-          </ul>
-          <h3>Chess</h3>
-          <p>about chess brief</p>
-          <h3>Starcraft: Brood War</h3>
-          <p>about, brief</p>
-          <h3>Books</h3>
-          <ul>
-            list
-            <li>book 1</li>
-            <li>book 2</li>
-          </ul>
+          <p>Briefly introducing myself.</p>
         </Col>
       </Row>
+
+      <h2>Learning software development</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae quae
+        veniam delectus quo voluptates, pariatur nulla repudiandae hic corrupti
+        itaque eveniet. Corporis voluptate dolorum iure neque veritatis illum
+        atque quasi!
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae quae
+        veniam delectus quo voluptates, pariatur nulla repudiandae hic corrupti
+        itaque eveniet. Corporis voluptate dolorum iure neque veritatis illum
+        atque quasi!Why
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae quae
+        veniam delectus quo voluptates, pariatur nulla repudiandae hic corrupti
+        itaque eveniet. Corporis voluptate dolorum iure neque veritatis illum
+        atque quasi!How did it go
+      </p>
+      <h2>Interests in the world of software development</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae quae
+        veniam delectus quo voluptates, pariatur nulla repudiandae hic corrupti
+        itaque eveniet. Corporis voluptate dolorum iure neque veritatis illum
+        atque quasi!Mention DS & ALGO
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae quae
+        veniam delectus quo voluptates, pariatur nulla repudiandae hic corrupti
+        itaque eveniet. Corporis voluptate dolorum iure neque veritatis illum
+        atque quasi!Geometries & gestalts
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae quae
+        veniam delectus quo voluptates, pariatur nulla repudiandae hic corrupti
+        itaque eveniet. Corporis voluptate dolorum iure neque veritatis illum
+        atque quasi!Dev rate
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae quae
+        veniam delectus quo voluptates, pariatur nulla repudiandae hic corrupti
+        itaque eveniet. Corporis voluptate dolorum iure neque veritatis illum
+        atque quasi!Conceptual exploration
+      </p>
+      <h2>Other interests</h2>
+      <h3>Poetry</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae quae
+        veniam delectus quo voluptates, pariatur nulla repudiandae hic corrupti
+        itaque eveniet. Corporis voluptate dolorum iure neque veritatis illum
+        atque quasi!brief intro
+      </p>
+      <ul>
+        <li>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae quae
+          veniam delectus quo voluptates, pariatur nulla repudiandae hic
+          corrupti itaque eveniet. Corporis voluptate dolorum iure neque
+          veritatis illum atque quasi!brief introPoem 1
+        </li>
+        <li>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae quae
+          veniam delectus quo voluptates, pariatur nulla repudiandae hic
+          corrupti itaque eveniet. Corporis voluptate dolorum iure neque
+          veritatis illum atque quasi!brief introPoem 2
+        </li>
+        <li>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae quae
+          veniam delectus quo voluptates, pariatur nulla repudiandae hic
+          corrupti itaque eveniet. Corporis voluptate dolorum iure neque
+          veritatis illum atque quasi!brief introPoem 3
+        </li>
+      </ul>
+      <h3>Chess</h3>
+      <p>about chess brief</p>
+      <h3>Starcraft: Brood War</h3>
+      <p>about, brief</p>
+      <h3>Books</h3>
+      <ul>
+        list
+        <li>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae quae
+          veniam delectus quo voluptates, pariatur nulla repudiandae hic
+          corrupti itaque eveniet. Corporis voluptate dolorum iure neque
+          veritatis illum atque quasi!brief introbook 1
+        </li>
+        <li>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae quae
+          veniam delectus quo voluptates, pariatur nulla repudiandae hic
+          corrupti itaque eveniet. Corporis voluptate dolorum iure neque
+          veritatis illum atque quasi!brief introbook 2
+        </li>
+      </ul>
     </Container>
   );
 }
