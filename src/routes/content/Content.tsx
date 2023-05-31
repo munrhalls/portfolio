ff222825aa5d642e4c57fe469644d39105d673fb
John <antarcticdepths@gmail.com>
2023-05-31 18:56:58 +0200

    content page

--- src/routes/content/Content.tsx
+++ src/routes/content/Content.tsx
@@ -1,15 +1,22 @@
+import { Container, Row, Col } from "react-bootstrap";
 import { articlesData } from "../../assets/articles/articlesData1-10";
 import Recording from "./Recording";
 
 function Articles() {
   console.log(articlesData);
   return (
-    <>
-      {articlesData.map((article) => {
-        return <>{article}</>;
-      })}
-      <Recording />
-    </>
+    <Container>
+      <Row>
+        <Col>
+          {articlesData.map((article) => {
+            return <>{article}</>;
+          })}
+        </Col>
+        <Col>
+          <Recording />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
