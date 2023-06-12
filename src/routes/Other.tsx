11e0d58fa326f3d37434d179437c26c8a08852fb
John <antarcticdepths@gmail.com>
2023-06-12 17:16:17 +0200

    poem images

--- src/routes/Other.tsx
+++ src/routes/Other.tsx
@@ -5,31 +5,33 @@ import { poemsData } from "./../../public/assets/poems/poemsData";
 import { useMediaQuery } from "react-responsive";
 import styled from "styled-components";
 
-const BgProvider = styled.div`
+const RowBGProvider = styled(Row)`
   position: relative;
+  overflow: hidden;
   &:before {
     z-index: 0;
     position: absolute;
     content: " ";
     top: : 0;
-    background: url(./public/assets/poems/poemImages/liquidAirAbuserImage.png) 5% 5% repeat;
-    height: 400%;
+    background: url(./public/assets/poems/poemImages/liquidAirAbuserImage.png);
+    background-repeat: repeat;
+    height: 150%;
     width: 100%;
-    animation: slide 30s ease-out infinite;
+    animation: slide 3s linear infinite;
     @keyframes slide {
       from {
-        top: -25%;
+        top: -40%;
       } 
       to {
-        top: 25%;
+        top: 0;
       }
     }
   }
 `;
 
 const PoemsTitle = styled.h1`
+  font-size: 2.25rem;
   color: #fff;
-  font-size: 2.75rem;
 `;
 
 function Other() {
@@ -43,36 +45,33 @@ function Other() {
         background: "#000",
       }}
     >
-      <Row className="justify-content-center mt-3 mb-5">
-        <Col className="mt-3" xs={7} sm={5} md={4} lg={4} xl={2}>
+      <Row className="justify-content-center">
+        <Col xs={7} sm={5} md={4} lg={4} xl={2}>
           <PoemsTitle>Poems</PoemsTitle>
         </Col>
       </Row>
 
       {poemsData.map((poem, index) => {
         return (
-          <BgProvider key={poem.title}>
-            <Row className="justify-content-center pt-5">
-              <>
-                <Col
-                  style={{
-                    position: "relative",
-                    borderRadius: "5px",
-                    boxShadow: "0 0 16px 48px #fff",
-                    background: "#fff",
-                  }}
-                  xs={7}
-                  sm={5}
-                  md={4}
-                  lg={4}
-                  xl={2}
-                >
-                  {poem.content}
-                </Col>
-              </>
-            </Row>
+          <>
+            <RowBGProvider key={poem.title} className="justify-content-center">
+              <Col
+                style={{
+                  position: "relative",
+                  borderRadius: "5px",
+                  background: "#fff",
+                }}
+                xs={7}
+                sm={5}
+                md={4}
+                lg={4}
+                xl={2}
+              >
+                {poem.content}
+              </Col>
+            </RowBGProvider>
             <hr style={{ marginTop: "12rem", marginBottom: "12rem" }} />
-          </BgProvider>
+          </>
         );
       })}
     </Container>
