476571a769192322fdcf8f3c126d0b0b1f702595
John <antarcticdepths@gmail.com>
2023-06-12 11:45:24 +0200

    look improvements

--- src/routes/Other.tsx
+++ src/routes/Other.tsx
@@ -31,25 +31,80 @@ function Other() {
         </Col>
       </Row>
 
-      {poemsData.map((poem) => (
+      {poemsData.map((poem, index) => (
         <Fragment key={poem.title}>
           <Row className="justify-content-around">
-            <Col
-              className="pt-3 pt-3"
-              style={{
-                position: "relative",
-                borderRadius: "5px",
-                boxShadow: "0 0 50px 100px #fff",
-                background: "#fff",
-              }}
-              xs={7}
-              sm={5}
-              md={4}
-              lg={4}
-              xl={2}
-            >
-              {poem.content}
-            </Col>
+            {index % 2 === 0 ? (
+              <>
+                <Col
+                  className="pt-3 pt-3"
+                  style={{
+                    position: "relative",
+                    borderRadius: "5px",
+                    boxShadow: "0 0 50px 100px #fff",
+                    background: "#fff",
+                  }}
+                  xs={7}
+                  sm={5}
+                  md={4}
+                  lg={4}
+                  xl={2}
+                >
+                  {poem.content}
+                </Col>
+                <Col
+                  className="pt-3 pt-3"
+                  style={{
+                    position: "relative",
+                    borderRadius: "5px",
+                    boxShadow: "0 0 50px 100px #fff",
+                    background: "#fff",
+                  }}
+                  xs={5}
+                  sm={5}
+                  md={4}
+                  lg={4}
+                  xl={2}
+                >
+                  <h1>ALTERNATING LEFT</h1>
+                </Col>
+              </>
+            ) : (
+              <>
+                <Col
+                  className="pt-3 pt-3"
+                  style={{
+                    position: "relative",
+                    borderRadius: "5px",
+                    boxShadow: "0 0 50px 100px #fff",
+                    background: "#fff",
+                  }}
+                  xs={5}
+                  sm={5}
+                  md={4}
+                  lg={4}
+                  xl={2}
+                >
+                  <h1>ALTERNATING RIGHT</h1>
+                </Col>
+                <Col
+                  className="pt-3 pt-3"
+                  style={{
+                    position: "relative",
+                    borderRadius: "5px",
+                    boxShadow: "0 0 50px 100px #fff",
+                    background: "#fff",
+                  }}
+                  xs={7}
+                  sm={5}
+                  md={4}
+                  lg={4}
+                  xl={2}
+                >
+                  {poem.content}
+                </Col>
+              </>
+            )}
           </Row>
           <hr style={{ marginTop: "12rem", marginBottom: "12rem" }} />
         </Fragment>
